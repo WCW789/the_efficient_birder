da2322c253e7b9e7696b3c412fcb32283814eb55
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-03-18 16:47:55 -0500

    Added Image scaffolding and fixed routing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_18_204734) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_18_211119) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_204734) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_birds_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "bird_id", null: false
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bird_id"], name: "index_images_on_bird_id"
+    t.index ["user_id"], name: "index_images_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_18_204734) do
   end
 
   add_foreign_key "birds", "users"
+  add_foreign_key "images", "birds"
+  add_foreign_key "images", "users"
 end
