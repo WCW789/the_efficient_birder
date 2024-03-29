e4702d58d570a93fabb3d8933ee183eec5563a02
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-03-29 01:44:12 -0500

    cleaned up UI

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -34,16 +34,13 @@ class Bird < ApplicationRecord
 
   def acceptable_image
     return unless image.attached?
-
+  
+    acceptable_types = ["image/jpeg", "image/gif", "image/png"]
+  
     image.each do |img|
       if img.blob.byte_size > 10.megabytes
         errors.add(:img, "is too big")
-      end
-    end
-
-    acceptable_types = ["image/jpeg", "image/gif", "image/png"]
-    image.each do |img|
-      unless acceptable_types.include?(img.content_type)
+      elsif !acceptable_types.include?(img.content_type)
         errors.add(:img, "must be a JPEG, GIF, or PNG")
       end
     end
