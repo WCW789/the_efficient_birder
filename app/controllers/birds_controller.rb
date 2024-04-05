bc037003b5760c89ef561a932ce995807c4d6277
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-04-05 16:07:36 -0500

    refactoring

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 require 'rest-client'
 require 'json'
+require 'aws-sdk-s3'
 
 class BirdsController < ApplicationController
   # include ApplicationHelper
@@ -50,68 +51,172 @@ class BirdsController < ApplicationController
     blob_field = params[:blob_field]
     latitude = params[:latitude]
     longitude = params[:longitude]
-
-    if uploaded_file.present?
-      File.open(Rails.root.join('tmp', 'uploads', uploaded_file.original_filename), 'wb') do |file|
-        file.write(uploaded_file.read)
-      end
-    end
-
-    image_path = Rails.root.join('tmp', 'uploads', 'snapshot.jpeg')
-    puts "image_path photo #{image_path}"
-    uploader = ImageUploader.new(image_path, ENV['S3_BUCKET'])
-    
-    @bird = current_user.bird.build(user_id: params[:user_id], name: params[:name], datetime: params[:datetime], notes: params[:notes], latitude: params[:latitude], longitude: params[:longitude])
-
-    puts "new bird photo #{@bird}"
-    
-    respond_to do |format|
-      if @bird.save
-        format.html { redirect_to bird_url(@bird), notice: "Bird was successfully uploaded." }
-        format.json { render :show, status: :created, location: @bird }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @bird.errors, status: :unprocessable_entity }
-      end
-    end
-
-    url = ENV['FLASK']
-
-    puts "url_photo #{url}"
-    
-    bucket_name = ENV['S3_BUCKET']
-    aws_region = ENV['AWS_REGION']
-
-    s3_object_url = uploader.upload()
-    puts " s3_object_url photo #{s3_object_url}"
-    data = { url: s3_object_url }
-
-    @response = RestClient.post(url, data.to_json, content_type: :json)
-    puts " response photo #{@response}"
-    @response_body = @response.body
-
-    puts "response_body_photo #{@response_body}"
-
-    @bird.name = @response_body
-    @bird.datetime = Time.new
-
-    base64_data = blob_field.split(',')[1] 
-    binary_data = Base64.strict_decode64(base64_data)
-    
-    filename = "image_#{Time.current.to_i}.jpg"
-    content_type = 'image/jpeg'
-    image_blob = ActiveStorage::Blob.create_and_upload!(
-      io: StringIO.new(binary_data),
-      filename: filename,
-      content_type: content_type
+  
+    @bird = current_user.bird.build(
+      user_id: params[:user_id],
+      name: params[:name],
+      datetime: params[:datetime],
+      notes: params[:notes],
+      latitude: params[:latitude],
+      longitude: params[:longitude]
     )
-    
-    @bird.image.attach(image_blob)
-    @bird.latitude = latitude
-    @bird.longitude = longitude
-    
-    @bird.save
+  
+    puts "new bird photo #{@bird}"
+  
+    url = ENV['FLASK']
+    puts "url_photo #{url}"
+  
+    if uploaded_file.present?
+      puts "upload_file"
+      s3 = Aws::S3::Resource.new
+      bucket = s3.bucket(ENV['S3_BUCKET'])
+      object_key = "#{SecureRandom.uuid}"
+      obj = bucket.object(object_key)
+      puts "obj"
+      obj.put(body: uploaded_file.read)
+      image_url = obj.public_url
+      puts "image_url1 #{obj.public_url}"
+
+      @data = { url: image_url }
+      @response = RestClient.post(url, @data.to_json, content_type: :json)
+      puts " response photo #{@response}"
+      @response_body = @response.body
+      @bird.name = @response_body
+
+      puts "@bird.name #{@bird.name}"
+
+      @bird.datetime = Time.now
+  
+      if blob_field.present?
+        base64_data = blob_field.split(',')[1]
+        binary_data = Base64.strict_decode64(base64_data)
+  
+        filename = "image_#{Time.current.to_i}.jpg"
+        content_type = 'image/jpeg'
+        image_blob = ActiveStorage::Blob.create_and_upload!(
+          io: StringIO.new(binary_data),
+          filename: filename,
+          content_type: content_type
+        )
+  
+        @bird.image.attach(image_blob)
+      end
+  
+      @bird.latitude = latitude
+      @bird.longitude = longitude
+
+      @bird.save
+  
+      respond_to do |format|
+        if @bird.save
+          key = @bird.image.first.key
+          format.html { redirect_to bird_url(@bird), notice: "Bird was successfully created." }
+          format.json { render :show, status: :created, location: @bird }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @bird.errors, status: :unprocessable_entity }
+        end
+      end
+    end
   end
+
+  # def save_photo
+  #   uploaded_file = params[:blobb]
+  #   blob_field = params[:blob_field]
+  #   latitude = params[:latitude]
+  #   longitude = params[:longitude]
+
+  #   @bird = current_user.bird.build(user_id: params[:user_id], name: params[:name], datetime: params[:datetime], notes: params[:notes], latitude: params[:latitude], longitude: params[:longitude])
+
+  #   puts "new bird photo #{@bird}"
+
+  #   url = ENV['FLASK']
+  #   puts "url_photo #{url}"
+
+  #   if uploaded_file.present?
+  #     puts "upload_file"
+  #     # File.open(Rails.root.join('tmp', 'uploads', uploaded_file.original_filename), 'wb') do |file|
+  #     #   file.write(uploaded_file.read)
+  #     # end
+  #     s3 = Aws::S3::Resource.new
+  #     bucket = s3.bucket(ENV['S3_BUCKET'])
+  #     object_key = "#{SecureRandom.uuid}"
+  #     obj = bucket.object(object_key)
+  #     puts "obj" 
+  #     sleep(2)
+  #     obj.put(body: uploaded_file.read)
+  #     image_url = obj.public_url
+  #     puts "image_url1 #{obj.public_url}"
+
+  #     s3_object_url = image_url
+  #     puts " s3_object_url photo #{s3_object_url}"
+  #     @data = { url: s3_object_url }
+  #     @response = RestClient.post(url, @data.to_json, content_type: :json)
+  #     puts " response photo #{@response}"
+  #     @response_body = @response.body
+  
+  #     puts "response_body_photo #{@response_body}"
+  
+  #     @bird_name = @response_body
+  #     @bird.name = @bird_name
+      
+
+
+  #   puts "@bird.name!!!!! #{@bird.name}"
+  #   puts "image_url2 #{image_url}"
+
+  #   # image_path = Rails.root.join('tmp', 'uploads', 'snapshot.jpeg')
+  #   # puts "image_path photo #{image_path}"
+  #   # uploader = ImageUploader.new(image_path, ENV['S3_BUCKET'])
+    
+
+    
+  #   # bucket_name = ENV['S3_BUCKET']
+  #   # aws_region = ENV['AWS_REGION']
+
+  #   # s3_object_url = image_url
+  #   # puts " s3_object_url photo #{s3_object_url}"
+  #   # data = { url: s3_object_url }
+
+  #   # @response = RestClient.post(url, data.to_json, content_type: :json)
+  #   # puts " response photo #{@response}"
+  #   # @response_body = @response.body
+
+  #   # puts "response_body_photo #{@response_body}"
+
+  #   # @bird.name = @response_body
+  #   @bird.datetime = Time.new
+
+  #   base64_data = blob_field.split(',')[1] 
+  #   binary_data = Base64.strict_decode64(base64_data)
+    
+  #   filename = "image_#{Time.current.to_i}.jpg"
+  #   content_type = 'image/jpeg'
+  #   image_blob = ActiveStorage::Blob.create_and_upload!(
+  #     io: StringIO.new(binary_data),
+  #     filename: filename,
+  #     content_type: content_type
+  #   )
+    
+  #   @bird.image.attach(image_blob)
+  #   @bird.latitude = latitude
+  #   @bird.longitude = longitude
+    
+  #   @bird.save
+
+
+  #   respond_to do |format|
+  #     if @bird.save
+  #       format.html { redirect_to bird_url(@bird), notice: "Bird was successfully uploaded." }
+  #       format.json { render :show, status: :created, location: @bird }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @bird.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+
+  # end
+  # end
 
   # POST /birds or /birds.json
   def create
