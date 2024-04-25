985977181f0c0b30fdf50bd04067796f93dada64
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-04-25 00:45:31 -0500

    debugging prod2

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   get("/birds/camera", { :controller => "birds", :action => "take_camera" })
   post("/birds/camera", { :controller => "birds", :action => "save_camera" })
   get("/birds/export", { :controller => "birds", :action => "export"})
-  
+  get("/uploads/:filename", { :controller => "uploads", :action => "show", constraints: { filename: /[^\/]+/ } })
   resources :birds
 
 end
