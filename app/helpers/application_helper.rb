70d39da6d3a04a81e73c88d8fbefb5ffed68e6fb
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-04-25 00:19:21 -0500

    changes to landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def s3_object_keys(bucket_name)
-    s3 = Aws::S3::Resource.new
-    bucket = s3.bucket(bucket_name)
-    last_object_key = bucket.objects.sort_by(&:last_modified).map(&:key)
-    last_object_key.last
-  end
 end
