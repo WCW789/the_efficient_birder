96b8b2ff8aa2a571f85493b70dce4557e87f2668
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-04-28 21:24:33 -0500

    working on adding demo video

--- app/controllers/uploads_controller.rb
+++
@@ -1,6 +0,0 @@
-class UploadsController < ApplicationController
-  def show
-    video_path = Rails.root.join('public', 'uploads', 'TestVideo.mp4')
-    send_file video_path, type: 'video/mp4', disposition: 'inline'
-  end
-end
