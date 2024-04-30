9aac7b4e9ff20dd86ba1a7bf1b4ffffa54cfbad2
WCW789 <32531807+WCW789@users.noreply.github.com>
2024-04-30 00:19:31 -0500

    removing unneeded code

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-  ],
-  plugins: [
-  ]
-}
+// module.exports = {
+//   content: [
+//     './public/*.html',
+//     './app/helpers/**/*.rb',
+//     './app/javascript/**/*.js',
+//     './app/views/**/*.{erb,haml,html,slim}',
+//   ],
+//   plugins: [
+//   ]
+// }
